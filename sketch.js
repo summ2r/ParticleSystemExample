4ad907a0d7fcebf5fab52fea6594184d44940799
summ2r <out08g16@naver.com>
2024-11-12 14:45:01 +0900

    tidy code

--- sketch.js
+++ sketch.js
@@ -12,27 +12,27 @@ function draw() {
 }
 
 // 간단한 파티클 클래스
-let Particle = function(position) {
+let Particle = function (position) {
   this.acceleration = createVector(0, 0.05);
   this.velocity = createVector(random(-1, 1), random(-1, 0));
   this.position = position.copy();
   this.lifespan = 255;
 };
 
-Particle.prototype.run = function() {
+Particle.prototype.run = function () {
   this.update();
   this.display();
 };
 
 // 위치 업데이트를 위한 메소드
-Particle.prototype.update = function(){
+Particle.prototype.update = function () {
   this.velocity.add(this.acceleration);
   this.position.add(this.velocity);
   this.lifespan -= 2;
 };
 
 // 화면에 보이기 위한 메소드
-Particle.prototype.display = function() {
+Particle.prototype.display = function () {
   stroke(200, this.lifespan);
   strokeWeight(2);
   fill(127, this.lifespan);
@@ -40,21 +40,21 @@ Particle.prototype.display = function() {
 };
 
 // 파티클이 여전히 쓸만한가요?
-Particle.prototype.isDead = function(){
+Particle.prototype.isDead = function () {
   return this.lifespan < 0;
 };
 
-let ParticleSystem = function(position) {
+let ParticleSystem = function (position) {
   this.origin = position.copy();
   this.particles = [];
 };
 
-ParticleSystem.prototype.addParticle = function() {
+ParticleSystem.prototype.addParticle = function () {
   this.particles.push(new Particle(this.origin));
 };
 
-ParticleSystem.prototype.run = function() {
-  for (let i = this.particles.length-1; i >= 0; i--) {
+ParticleSystem.prototype.run = function () {
+  for (let i = this.particles.length - 1; i >= 0; i--) {
     let p = this.particles[i];
     p.run();
     if (p.isDead()) {

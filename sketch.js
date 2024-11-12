19b4b9c8bc4d9d23af5347b218fbd1688eee76f7
summ2r <out08g16@naver.com>
2024-11-12 15:39:59 +0900

    wind 추가

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 let system;
 let g; //gravity
+let wind;
 
 function setup() {
   createCanvas(720, 400);
   system = new ParticleSystem(createVector(width / 2, 50));
   g = createVector(0, 0.05);
+  wind = createVector(0.03, -0.01);
 }
 
 function draw() {

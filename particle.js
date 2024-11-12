43274295e5cdba87c65abbbb22e68d776342bbcb
summ2r <out08g16@naver.com>
2024-11-12 15:13:54 +0900

    particle 수정

--- particle.js
+++ particle.js
@@ -1,5 +1,6 @@
 class Particle {
-    constructor() {
+    
+    constructor(position) {
     this.acceleration = createVector(0, 0.05);
     this.velocity = createVector(random(-1, 1), random(-1, 0));
     this.position = position.copy();

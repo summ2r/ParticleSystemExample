2e6354c028a58755c4c1608a9d8aba820327a945
summ2r <out08g16@naver.com>
2024-11-12 15:51:28 +0900

    system 여러 개 만들기 (객체 지향)

--- sketch.js
+++ sketch.js
@@ -1,18 +1,29 @@
 let system;
+let systems = [];
+
 let g; //gravity
 let wind;
 
 function setup() {
   createCanvas(720, 400);
-  system = new ParticleSystem(createVector(width / 2, 50));
+  // system = new ParticleSystem(createVector(width / 2, 50));
   g = createVector(0, 0.05);
   wind = createVector(0.03, -0.01);
 }
 
 function draw() {
   background(51);
-  system.addParticle();
-  system.applyGravity(g);
-  system.applyForce(wind);
-  system.run();
+
+  for (s of systems) {
+    s.addParticle();
+    s.applyGravity(g);
+    system.applyForce(wind);
+    system.run();
+  }
+}
+
+function mouseClicked() {
+  let mPos = createVector(mouseX, mouseY);
+  let system = new ParticleSystem(mPos);
+  systems.push(system);
 }

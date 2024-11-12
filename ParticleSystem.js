fa2774bb100461f6a929ee7f1e975f60e87cba77
summ2r <out08g16@naver.com>
2024-11-12 15:37:59 +0900

    중력을 system에 적용

--- ParticleSystem.js
+++ ParticleSystem.js
@@ -8,6 +8,12 @@ class ParticleSystem {
         this.particles.push(new Particle(this.origin));
     }
 
+    applyGravity(g) {
+        for (let p of this.particles) {
+            p.applyForce(g);
+        }
+    }
+
     run() {
         for (let i = this.particles.length - 1; i >= 0; i--) {
             let p = this.particles[i];
--- sketch.js
+++ sketch.js
@@ -1,12 +1,15 @@
 let system;
+let g; //gravity
 
 function setup() {
   createCanvas(720, 400);
   system = new ParticleSystem(createVector(width / 2, 50));
+  g = createVector(0, 0.05);
 }
 
 function draw() {
   background(51);
   system.addParticle();
+  system.applyGravity(g);
   system.run();
 }
